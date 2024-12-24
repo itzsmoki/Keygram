9c99e61ebae04cac71abf86d06d13735b56e4bfc
Smoki <150018185+itzsmoki@users.noreply.github.com>
2024-12-24 17:25:16 +0000

    update

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
-let SECRET
-let SERVER
+let SECRET;
+let SERVER;
 const send = chrome.i18n.getMessage("send");
 
 function updateStorageValues() {
@@ -12,60 +12,61 @@ function updateStorageValues() {
   });
 }
 
+updateStorageValues();
+setInterval(updateStorageValues, 500);
 
 const onStreamKeyBoxLoaded = () => {
   const streamKeyBox = document.querySelector(
     'input[name="live-creation-modal-start-pane-stream-key"]'
-  )
+  );
   if (streamKeyBox) {
     let sendButton = streamKeyBox.parentNode.querySelector(
       'button.send-stream-key'
-    )
+    );
     if (!sendButton) {
-      sendButton = document.createElement('button')
-      sendButton.textContent = send
-      sendButton.classList.add('send-stream-key')
-      sendButton.style.marginLeft = '2px'
-      sendButton.style.backgroundColor = 'transparent'
-      sendButton.style.color = '#0095f6'
-      sendButton.style.border = 'none'
-      sendButton.style.fontWeight = '500'
+      sendButton = document.createElement('button');
+      sendButton.textContent = send;
+      sendButton.classList.add('send-stream-key');
+      sendButton.style.marginLeft = '2px';
+      sendButton.style.backgroundColor = 'transparent';
+      sendButton.style.color = '#0095f6';
+      sendButton.style.border = 'none';
+      sendButton.style.fontWeight = '500';
 
       sendButton.addEventListener('mouseover', () => {
         if (!sendButton.disabled) {
-          sendButton.style.color = 'white'
+          sendButton.style.color = 'white';
         }
-      })
+      });
 
       sendButton.addEventListener('mouseout', () => {
         if (!sendButton.disabled) {
-          sendButton.style.color = '#0095f6'
+          sendButton.style.color = '#0095f6';
         }
-      })
+      });
 
       sendButton.addEventListener('mousedown', () => {
-        sendButton.style.color = 'gray'
-      })
+        sendButton.style.color = 'gray';
+      });
 
       sendButton.addEventListener('mouseup', () => {
-        sendButton.style.color = 'white'
-      })
+        sendButton.style.color = 'white';
+      });
 
       sendButton.addEventListener('click', () => {
-        sendButton.disabled = true
-        sendButton.style.color = 'gray'
-      })
+        sendButton.disabled = true;
+        sendButton.style.color = 'gray';
+      });
 
-      streamKeyBox.parentNode.appendChild(sendButton)
+      streamKeyBox.parentNode.appendChild(sendButton);
 
       sendButton.addEventListener('click', () => {
-        const streamKey = streamKeyBox.value
-        updateStorageValues();
+        const streamKey = streamKeyBox.value;
 
-        if (!streamKey) {
-          sendButton.disabled = false
-          sendButton.style.color = '#0095f6'
-          return
+        if (!streamKey || !SERVER) {
+          sendButton.disabled = false;
+          sendButton.style.color = '#0095f6';
+          return;
         }
 
         fetch(SERVER, {
@@ -75,29 +76,28 @@ const onStreamKeyBoxLoaded = () => {
         })
           .then(response => {
             if (!response.ok) {
-              throw new Error(response.status)
+              throw new Error(response.status);
             }
-            return response.text()
+            return response.text();
           })
           .then(data => {
           })
           .catch(error => {
-            sendButton.disabled = false
-            sendButton.style.color = '#0095f6'
-          })
-      })
+            sendButton.disabled = false;
+            sendButton.style.color = '#0095f6';
+          });
+      });
     }
-  } else {
   }
-}
+};
 
 const observer = new MutationObserver((mutationsList, observer) => {
   const streamKeyBox = document.querySelector(
     'input[name="live-creation-modal-start-pane-stream-key"]'
-  )
+  );
   if (streamKeyBox) {
-    onStreamKeyBoxLoaded()
+    onStreamKeyBoxLoaded();
   }
-})
+});
 
-observer.observe(document.body, { childList: true, subtree: true })
+observer.observe(document.body, { childList: true, subtree: true });
